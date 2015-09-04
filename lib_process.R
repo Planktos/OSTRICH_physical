d6e4de4a5ba3f0229effc7352f0e17a318d918f4
Planktos <kellylynnrobinson@gmail.com>
2015-09-04 10:19:27 -0700

    Lines 199-203: midnight shift code on Line 203 was not working for all files so commented it out
    Line 210-215: added code to adjust date based on midnight shift in time column (from JL 'lib_process.R'
    
    Line 217: removed uneeded date field after creation of dateTime field

--- lib_process.R
+++ lib_process.R
@@ -174,7 +174,7 @@ phyFiles <- list.files("raw_physical_data_2014", full=TRUE)
 phy <- adply(phyFiles, 1, function(file) {
   
   # read the data
-  d <- read.table(file, sep="\t", skip=10, header=TRUE, fileEncoding="ISO-8859-1", stringsAsFactors=FALSE, quote="\"", check.names=FALSE, encoding="UTF-8", na.strings="9999.99")
+  d <- read.table(phyFiles[64], sep="\t", skip=10, header=TRUE, fileEncoding="ISO-8859-1", stringsAsFactors=FALSE, quote="\"", check.names=FALSE, encoding="UTF-8", na.strings="9999.99")
   
   # clean names
   head <- names(d)
@@ -187,7 +187,7 @@ phy <- adply(phyFiles, 1, function(file) {
   names(d) <- head
   
   # create a proper date + time format
-  date <- scan(file, what="character", skip=1, nlines=1, quiet=TRUE)
+  date <- scan(phyFiles[64], what="character", skip=1, nlines=1, quiet=TRUE)
   date <- date[2]
   mm <- str_sub(date,1,2)
   dd <- str_sub(date,4,5)
@@ -196,19 +196,25 @@ phy <- adply(phyFiles, 1, function(file) {
   dateNextDay <- str_c(mm,as.character(dd+1),yy, sep="/")
   
   # shift by one day when we cross midnight
-  d1$hour <- as.numeric(str_sub(d1$time,1,2))
-  d1$date <- date
-  #d$date <- ifelse(d$hour >= 18 & d$hour <= 23, date, dateNextDay) #San Diego range -kr
-  #Straits of Florida range -kr
-  d$date <- ifelse(d$hour >= 20 & d$hour <= 23, date, dateNextDay)
+  d$hour <- as.numeric(str_sub(d$time,1,2))
+  d$date <- date
+  #d$time <- as.POSIXct(strptime(d$time, format= "%H:%M:%OS", tz = "America/New_York"))
+  #d$date <- ifelse(d$hour >= 18 & d$hour <= 23, date, dateNextDay) #San Diego midnight shift code - kr
   d$dateTime <- str_c(d$date, d$time, sep=" ")
   d$dateTime <- as.POSIXct(strptime(d$dateTime, format="%m/%d/%y %H:%M:%OS", tz="America/New_York"))
   # Set time zone of collection location
   # NB: we say it is America/New York when it is in fact local time, just to avoid having to deal with time zones
-  
   # shift time of all physical data if not collected in Eastern Time Zone by appropriate number of hourse
   # if collected in the Eastern Time Zone, then set change value to 0
-  d$dateTime <- d$dateTime - 0 * 3600  
+  d$dateTime <- d$dateTime - 0 * 3600 
+  
+  # detect midnight shifts
+  midnightShift <- which(diff(d$dateTime) < 0)
+  if (length(midnightShift) > 0) {
+    d$dateTime[midnightShift:nrow(d)] <- d$dateTime[midnightShift:nrow(d)] + 24 * 3600
+  }
+  
+  d$date <- NULL
   
   # code in a transect number
   # use the file name as a dummy variable for transect number. Will assign proper transect number later in the pipeline.
@@ -257,7 +263,7 @@ phy <- adply(phyFiles, 1, function(file) {
                    "irrandiance"="irradiance"))
   
   # keep only interesting data
-  d <- d[,c("transect", "dateTime", "depth", "lat", "long", "temp", "salinity", "pressure", "fluoro", "oxygen", "irradiance", "heading", "horizontal.vel", "vertical.vel", "pitch", "vol.imaged")]
+  d <- d[,c("transect", "dateTime","depth", "lat", "long", "temp", "salinity", "pressure", "fluoro", "oxygen", "irradiance", "heading", "horizontal.vel", "vertical.vel", "pitch", "vol.imaged")]
   
   return(d)
   head
