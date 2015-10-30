b2b4ef3eec40c96a39220f732edfc201cc410aea
Planktos <kellylynnrobinson@gmail.com>
2015-10-29 21:08:14 -0700

    loops to prep netCDF data for plots & ArcGIS

--- adcp_process.R
+++ adcp_process.R
@@ -1,21 +1,15 @@
-#process ADCP data
-
-library(stringr)
-library(plyr)
-library(dplyr)
-
+library("chron")
+library("lattice")
+library("RNetCDF")
+library("plyr")
+library("lubridate")
+library("stringr")
 
 options("digits.secs" = 3)
 
-u <- read.csv("u_test.csv",stringsAsFactors = F, header = F)
-v <- read.csv("v_test.csv",stringsAsFactors = F, header = F)
-xyt <- read.csv("current_xyt.csv",stringsAsFactors = F, header = F)
-z <- read.csv("depth_adcp_test.csv",stringsAsFactors = F, header = F)
-
-dir.create("temp")
-
-#function-----
-adcp <- function(u,v,z,yxt){
+#functions
+#-----
+adcp_bin <- function(u,v,z,yxt){ #used for ADCP_test (currently stored in bin folder)
   for (i in 1:length(u)){
     u_temp <- u[,i]
     v_temp <- v[,i]
@@ -29,81 +23,148 @@ adcp <- function(u,v,z,yxt){
     write.csv(d,file=paste0("temp/d",i,".csv"),row.names = FALSE)
   }
 }
-#------
-#run function
-adcp(u,v,z,xyt)
 
-setwd("C:/Users/kelly.robinson/Dropbox/Cowen_Sponaugle_share/OSTRICH/ISIIS data/OSTRICH_physical/temp")
-files <- list.files("C:/Users/kelly.robinson/Dropbox/Cowen_Sponaugle_share/OSTRICH/ISIIS data/OSTRICH_physical/temp")
+adcp <- function(u,v,z,lat,lon,time,flag){
+  for (i in 1:length(u)){
+    u_temp <- u[,i]
+    v_temp <- v[,i]
+    z_temp <- z[,i]
+    flag_temp <- flag[,i]
+    uvz <- cbind(z_temp, u_temp, v_temp, flag_temp)
+    
+    lat_temp <- lat[i,]
+    lon_temp <- lon [i,]
+    doy <- time[i,]
+    
+    c <- cbind(doy, lat_temp, lon_temp, uvz)
+    c <- as.data.frame(c)
+    c <- c[order(z_temp), ]
+    
+    #rename columns
+    names(c)[names(c)=="lat_temp"] <- "lat"
+    names(c)[names(c)=="lon_temp"] <- "lon"
+    names(c)[names(c)=="z_temp"] <- "depth[m]"
+    names(c)[names(c)=="u_temp"] <- "u.velocity[m/s]"
+    names(c)[names(c)=="v_temp"] <- "v.velocity[m/s]"
+    names(c)[names(c)=="flag_temp"] <- "qual.flag"
+    
+    write.csv(c, file=paste0("temp/c",i,".csv"), row.names = FALSE)
+  }
+}
 
+#--------
+#2014 data
+#--------
+setwd("C:/Users/kelly.robinson/Dropbox/Cowen_Sponaugle_share/OSTRICH/ISIIS data/OSTRICH_physical/ADCP_2014/")
 
-adcp_data <- adply(files, 1, function(file) {
-  t <- read.table(file, header=T, sep=",")
-  #name the fields
-  colnames(t) <- c("u","v","z_bin","lat","lon","doy")
-    return(t)
-}, .progress="text")
+#create temporary directory
+dir.create("temp")
+#create dictory for processed files
+dir.create("ship_currents")
 
-setwd("C:/Users/kelly.robinson/Dropbox/Cowen_Sponaugle_share/OSTRICH/ISIIS data/OSTRICH_physical")
+#--------
+#2015 data
+#--------
+setwd("C:/Users/kelly.robinson/Dropbox/Cowen_Sponaugle_share/OSTRICH/ISIIS data/OSTRICH_physical/ADCP_2015/")
 
-# remove adply crap
-adcp_data <- adcp_data[,-1]
+#create temporary directory
+dir.create("temp")
+#create dictory for processed files
+dir.create("ship_currents")
 
-d<-adcp_data
+#----
+#code
+#----
 
-  library(lubridate)
+#list sensor netCDF files
+f <- list.files(pattern = ".nc")
 
-  #create proper date
-  d$date<-strptime(x = d$doy, format ="%j", tz="GMT")
-  d$date<-as.POSIXct(d$date)
-  year <- year(d$date)
-  month <- month(d$date)
-  day <- day(d$date)
+#loop through sensor netCDF files
+for (i in 1:length(f)){
+  fname <- f[i]
   
-  #create proper time
-  t <- trunc(d$doy,3)
-  dec.day <- d$doy-t
-  dec.hh <- dec.day*24
-  hour <- as.integer(dec.hh)
-  dec.min <- dec.hh-hour
-  min <- as.integer(dec.min*60)
-  dec.sec <- (dec.min*60)-min
-  sec <- dec.sec*60
+  fid <- open.nc(fname)
   
-  #combine date and time
-  d$dateTime<-ISOdatetime(year,month,day,hour,min,sec, tz="GMT")
- 
-  #create time field in format yyyymmddhhminss.s for ArcMap
-  d$dateTime <- as.character(d$dateTime)
-  t <- str_split_fixed(d$dateTime, "-|:| ",6)
-  yy <- substring(t[,1],1)
-  mm <- substring(t[,2],1)
-  dd <- substring(t[,3],1)
-  hh <- substring(t[,4],1)
-  min <- substring(t[,5],1)
-  sec <- substring(t[,6],1)
-  d$time <- as.numeric(paste0(yy,mm,dd,hh,min,sec))
+  #read in data fields from netCDF file
+  dat<-read.nc(fid)
+    
+    u <- as.data.frame(dat$u)
+    v <- as.data.frame(dat$v)
+    lat <- as.data.frame(dat$lat)
+    lon <- as.data.frame(dat$lon)
+    time <- as.data.frame(dat$time)
+    z <- as.data.frame(dat$depth)
+    flag <- as.data.frame(dat$pflag)
   
-  #calculate current speed
-  d$current_speed <- sqrt(d$u^2+d$v^2)
-  
-  #calculate rotation
-  d$rotation <- (180/3.14)*atan2(d$u,d$v)
-  
-  #change sign of depth bin for ArcGIS
-  d$z_bin <- d$z_bin*-1
-  
-  #sort data by day of year (doy)
-  sort(d$doy)
-  
-  d <- na.omit(d)
-  
-  write.table(d, file="ost15_WS_currents.txt", sep="\t", row.names = F, col.names = T)
-  
+  #run function to create individual temp files for current velocities at each lat, lon, and time observation
+  adcp(u,v,z,lat,lon,time,flag)
 
+  files <- list.files("temp", full=T)
+  
+  adcp_data <- adply(files, 1, function(file) {
+    
+    d <- read.table(file, header=T, sep=",")
+   
+    #create proper date
+    d$date<-strptime(x = d$doy, format ="%j", tz="GMT")
+    d$date<-as.POSIXct(d$date)
+    year <- year(d$date)
+    month <- month(d$date)
+    day <- day(d$date)
+    
+    #create proper time
+    t <- trunc(d$doy,3)
+    dec.day <- d$doy-t
+    dec.hh <- dec.day*24
+    hour <- as.integer(dec.hh)
+    dec.min <- dec.hh-hour
+    min <- as.integer(dec.min*60)
+    dec.sec <- (dec.min*60)-min
+    sec <- dec.sec*60
+  
+    #combine date and time
+    d$dateTime<-ISOdatetime(year,month,day,hour,min,sec, tz="GMT")
+  
+    #create time field in format yyyymmddhhminss.s for ArcMap
+    d$dateTime <- as.character(d$dateTime)
+    t <- str_split_fixed(d$dateTime, "-|:| ",6)
+    yy <- substring(t[,1],1)
+    mm <- substring(t[,2],1)
+    dd <- substring(t[,3],1)
+    hh <- substring(t[,4],1)
+    min <- substring(t[,5],1)
+    sec <- substring(t[,6],1)
+    d$time <- as.numeric(paste0(yy,mm,dd,hh,min,sec))
+  
+    #calculate current speed
+    d$current_speed <- sqrt(d$u.velocity.m.s^2+d$v.velocity.m.s.^2)
+  
+    #calculate rotation
+    d$rotation <- (180/3.14)*atan2(d$u.velocity.m.s, d$v.velocity.m.s.)
+  
+    #change sign of depth bin for ArcGIS
+    d$depth.m. <- d$depth.m.*-1
+    
+    #sort data by day of year (doy)
+    sort(d$doy)
+    
+  return(d)
+  
+  }, .progress="text")
+  
+  # remove adply crap
+  adcp_data <- adcp_data[,-1]
+  
+  #remove obserations with non-zero quality flags
+  adcp_data_good <- subset(adcp_data, qual.flag == 0)
+  
+  name <- str_split_fixed(string = f[i], pattern = ".nc", n = 2)
+  filename <- substring(name[,1],1)
+  
+  write.table(adcp_data_good, file=paste0("ship_currents/",filename,".txt"), sep="\t", row.names = F, col.names = T)
+}
 
+#clean-up: delete temporary directory
+unlink("temp", recursive = T)
 
-
-
-
-
+            
